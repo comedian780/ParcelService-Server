dec1761e55e68331869c849fe7ce0e00148d20d1
SabertoothX6 <sabertoothx6@hotmail.de>
2018-06-04 15:03:55 +0200

    Changed dns

--- src/server/DBConn.java
+++ src/server/DBConn.java
@@ -13,7 +13,7 @@ public class DBConn {
     public String getSize(double gurt){
 	    try{
 		    Class.forName("com.mysql.cj.jdbc.Driver");
-		    Connection con=DriverManager.getConnection("jdbc:mysql://database:3306/ParcelService?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","reader","penis");
+		    Connection con=DriverManager.getConnection("jdbc:mysql://db:3306/ParcelService?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","reader","penis");
 		    Statement stmt=con.createStatement();
 		    ResultSet rs=stmt.executeQuery("select distinct size from ParcelSize where min <= " + gurt + " and max >= " + gurt);
 		    rs.next();
