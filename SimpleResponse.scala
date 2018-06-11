71532e1b2faa5eb0dceba6d51113134f0468eaf7
SabertoothX6 <sabertoothx6@hotmail.de>
2018-06-11 15:04:31 +0200

    Added scala testfile and request txt

--- SimpleResponse.scala
+++ SimpleResponse.scala
@@ -1,4 +1,4 @@
-package parcel
+package parcelService
 
 import scala.concurrent.duration._
 
@@ -9,43 +9,46 @@ import io.gatling.jdbc.Predef._
 class RecordedSimulation extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://localhost")
+		.baseURL("http://192.168.56.101:8443")
 		.inferHtmlResources()
 		.acceptHeader("*/*")
 		.acceptEncodingHeader("gzip, deflate")
 		.acceptLanguageHeader("en-GB,en;q=0.5")
 		.userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0")
 
-	val headers_3 = Map(
+	val headers_5 = Map(
 		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Access-Control-Request-Headers" -> "content-type",
 		"Access-Control-Request-Method" -> "POST",
-		"Origin" -> "http://192.168.56.101")
+		"Origin" -> "http://localhost")
 
-	val headers_4 = Map(
+	val headers_6 = Map(
 		"Accept" -> "application/json, text/plain, */*",
 		"Content-Type" -> "application/json;charset=utf-8",
-		"Origin" -> "http://192.168.56.101")
+		"Origin" -> "http://localhost")
 
     val uri1 = "http://192.168.56.100/js"
-    val uri2 = "192.168.56.101"
+    val uri2 = "http://192.168.56.101:8443/parcel/size"
 
 	val scn = scenario("RecordedSimulation")
 		.exec(http("request_0")
-			.get("/images/icon_pakete.png")
+			.get(uri1 + "/parcel-address.component.js")
 			.resources(http("request_1")
-			.get(uri1 + "/app.js"),
+			.get(uri1 + "/parcel-option.component.js"),
             http("request_2")
-			.get(uri1 + "/parcel-price.component.js"))
-			.check(status.is(404)))
-		.pause(16)
-		.exec(http("request_3")
-			.options("http://" + uri2 + ":8443/parcel/size")
-			.headers(headers_3)
-			.resources(http("request_4")
-			.post("http://" + uri2 + ":8443/parcel/size")
-			.headers(headers_4)
-			.body(RawFileBody("RecordedSimulation_0004_request.txt"))))
+			.get(uri1 + "/app.js"),
+            http("request_3")
+			.get(uri1 + "/parcel-price.component.js"),
+            http("request_4")
+			.get(uri1 + "/parcel-size.component.js")))
+		.pause(6)
+		.exec(http("request_5")
+			.options("/parcel/size")
+			.headers(headers_5)
+			.resources(http("request_6")
+			.post("/parcel/size")
+			.headers(headers_6)
+			.body(RawFileBody("SimpleResponse.txt"))))
 
 	setUp(scn.inject(atOnceUsers(100))).protocols(httpProtocol)
 }
