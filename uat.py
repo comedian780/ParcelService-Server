294aa0b0922ef9bc7212942a1123ed46f15d9b54
SabertoothX6 <sabertoothx6@hotmail.de>
2018-06-25 15:31:30 +0200

    print

--- uat.py
+++ uat.py
@@ -32,6 +32,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_id("depth").send_keys("3")
         driver.find_element_by_id("sendParcel").click()
         time.sleep(5)
+        print(driver.find_element_by_id("parcelRes").text)
         result = driver.find_element_by_id("parcelRes").text.split()[1]
         if(result != self.correct_result):
             exit(1)
